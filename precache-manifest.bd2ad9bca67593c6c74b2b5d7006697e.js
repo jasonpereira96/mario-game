495e7164825d8bd89a8c083f55fe95a5a0cb2ebf
jasonpereira96 <jasonpereira074@gmail.com>
2020-10-24 11:40:35 +0530

    Updates

---
+++ precache-manifest.bd2ad9bca67593c6c74b2b5d7006697e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89944bb480a936702ec798a65389c9d0",
+    "url": "/mario-game/index.html"
+  },
+  {
+    "revision": "12ec24ee3c54ed7245f3",
+    "url": "/mario-game/static/css/main.b6fd0eab.chunk.css"
+  },
+  {
+    "revision": "3a998fc88b7952f1bbfe",
+    "url": "/mario-game/static/js/2.d5555656.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/mario-game/static/js/2.d5555656.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12ec24ee3c54ed7245f3",
+    "url": "/mario-game/static/js/main.ff7924d5.chunk.js"
+  },
+  {
+    "revision": "3bd9d770666423ab97f1",
+    "url": "/mario-game/static/js/runtime-main.7295afe5.js"
+  },
+  {
+    "revision": "252ff07a27b7bee5b7a5dca352541e86",
+    "url": "/mario-game/static/media/mario.252ff07a.svg"
+  },
+  {
+    "revision": "7dc6876e9ca607d6842dc8864431d36e",
+    "url": "/mario-game/static/media/mushroom.7dc6876e.svg"
+  }
+]);
